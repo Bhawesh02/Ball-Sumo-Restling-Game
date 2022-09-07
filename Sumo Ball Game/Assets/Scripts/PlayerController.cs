1d6bbd974953cfb4b436d0e33f94e320f769a70c
Bhawesh02 <bhaweshagarwal02@gmail.com>
2022-09-07 13:52:37 +0530

    Player mover where camera is facing

--- Sumo Ball Game/Assets/Scripts/PlayerController.cs
+++ Sumo Ball Game/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5;
     private Rigidbody playerRb;
+    private GameObject focalPoint;
+    public float speed = 5;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * forwardInput * speed );
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);    ;
     }
 }
--- Sumo Ball Game/Assets/Scripts/RotateCamera.cs
+++ Sumo Ball Game/Assets/Scripts/RotateCamera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
-    private float rotationSpeed = 50f;
+    public float rotationSpeed = 45f;
     // Start is called before the first frame update
     void Start()
     {
